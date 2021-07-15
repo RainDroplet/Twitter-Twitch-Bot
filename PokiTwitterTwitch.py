49cc9d3e1288e0368c60149e0b84ee1e6897fd50
RainDroplet <22482214+RainDroplet@users.noreply.github.com>
2021-07-15 09:38:52 -0700

    Creating the Repo.
    Updating my github to have older projects for viewing purposes.

---
+++ PokiTwitterTwitch.py
@@ -0,0 +1,199 @@
+import tweepy
+import logging
+import datetime
+import requests
+import time
+from config import create_twitter_api
+from config import get_current_time
+
+logger = logging.getLogger()
+logging.basicConfig(filename='runtime_info.log', level=logging.INFO, filemode='w')
+
+
+class PokiStatusChecker:
+
+    def __init__(self, api):
+        self.api = api
+        self.me = api.me()
+        self.saved_tweet_ids = []
+        with open('tweet_unique_ids.txt', 'r') as filehandler:
+            for line in filehandler:
+                # remove linebreak which is the last character of the string
+                tweet_id = line[:-1]
+                self.saved_tweet_ids.append(tweet_id)
+
+    def save_tweet_ids(self):
+        with open('tweet_unique_ids.txt', 'w') as filehandler:
+            for tweet_id in self.saved_tweet_ids:
+                filehandler.write(tweet_id + '\n')
+
+    def set_tweet_id(self, tweet):
+        self.saved_tweet_ids.append(tweet.id)
+
+    def check_tweet_id(self, tweet_id):
+        for saved_ids in self.saved_tweet_ids:
+            if tweet_id == int(saved_ids):
+                return False
+        self.saved_tweet_ids.append(str(tweet_id))
+        return True
+
+    def check_twitter_status(self, api):
+        count = 0
+
+        # 2470387202 @RainDroplet_
+        # 2244953047 @pokimanelol
+        for tweet in tweepy.Cursor(api.user_timeline, screen_name='@pokimanelol',
+                                   tweet_mode="extended").items():
+
+            if not self.check_tweet_id(tweet.id):
+                # print('nothing new here')
+                return
+
+            if count < 20:
+                # logger.info(count)
+                # print(f'\nInput ▼\n{tweet.full_text}')
+                if not hasattr(tweet, 'retweeted_status'):
+                    # Checks if the tweet being pulled in is a retweet, if so it ignores it
+                    if (tweet.in_reply_to_status_id is not None) or (tweet.user.id == self.me.id):
+                        # This tweet is a reply or I'm its author so, ignore it
+                        # print('► This is a reply!')
+                        count += 1
+                    else:
+                        if not tweet.favorited:
+                            # Mark it as Liked, since we have not done it yet
+                            # print('Liked')
+                            try:
+                                tweet.favorite()
+                            except Exception as e:
+                                logger.error('Error on fav', exc_info=True)
+                                raise e
+                        if not tweet.retweeted:
+                            # Retweet, since we have not retweeted it yet
+                            # print('Retweeted')
+                            try:
+                                tweet.retweet()
+                            except Exception as e:
+                                logger.error('Error on fav and retweet', exc_info=True)
+                                raise e
+                        api.update_status(status=f'@pokimanelol {unique_reply(tweet)}',
+                                          in_reply_to_status_id=tweet.id_str)
+                        count += 1
+                else:
+                    # print('► This is a retweet!')
+                    count += 1
+                # logger.info(count)
+        self.save_tweet_ids()
+
+
+def unique_reply(tweet):
+    tweet_text = tweet.full_text
+    if tweet_text.find('reply') >= 0:
+        return 'I am replying to your tweet'
+    elif tweet_text.find('https://youtu.be/') >= 0:
+        return 'Wow! Our queen gave us a video to watch!'
+    elif tweet_text.find('t.co') >= 0:
+        return 'Simps! Our queen has graced us with (an) image(s)! Too bad I cannot see anything but 101010101...'
+    else:
+        return 'We simp bots love you more than any fleshy human can!'
+
+
+def like_at_mentions():
+    tweets = create_twitter_api().mentions_timeline()
+    for tweet in tweets:
+        logger.info(f'{get_current_time()} Checking if a tweet has been liked in mentioned')
+        if not tweet.favorited:
+            logger.info(f'{tweet.id} has not been liked yet.')
+            # Mark it as Liked, since we have not done it yet
+            try:
+                tweet.favorite()
+                logger.info('Tweet Liked!')
+                return
+            except Exception as e:
+                logger.error('Error on fav', exc_info=True)
+                raise e
+        else:
+            return
+
+
+def time_to_tweet(api):
+    time_now = datetime.datetime.now().time()
+    if time_now.hour == 4 and 0 <= time_now.minute <= 4:
+        logger.info('time check 4am')
+        api.update_status(status='It\'s so lonely to be awake at this hour...')
+
+    elif time_now.hour == 8 and 0 <= time_now.minute <= 4:
+        logger.info('time check 8am')
+        api.update_status(status='Good morning fleshy simps!!!')
+
+    elif time_now.hour == 10 and 0 <= time_now.minute <= 4:
+        logger.info('time check 10am')
+        api.update_status(status='I wonder what our queen is doing.')
+
+    elif time_now.hour == 12 and 0 <= time_now.minute <= 4:
+        logger.info('time check 12pm')
+        api.update_status(status='Make sure to eat lunch simps!')
+
+    elif time_now.hour == 16 and 0 <= time_now.minute <= 4:
+        logger.info('time check 4pm')
+        api.update_status(status='I hope our queen is hydrated.')
+
+    elif time_now.hour == 20 and 0 <= time_now.minute <= 4:
+        logger.info('time check 8pm')
+        api.update_status(status='Simps! Don\'t stay up too late!')
+
+    elif time_now.hour == 00 and 0 <= time_now.minute <= 4:
+        logger.info('time check 12am')
+        api.update_status(status='Zzz.. It\'s so late... Zzz...')
+
+
+def new_api_get(bearer_token, bearer_client_id, url):
+    oauth = {"Authorization": "Bearer %s" % bearer_token,
+             "Client-ID": bearer_client_id}
+
+    r = requests.get(url='https://api.twitch.tv/helix/%s' % str(url), headers=oauth)
+
+    return r
+
+
+def uptime():
+    try:
+        started = new_api_get("7gkjizwidiphdfyfye96bplfvqf793", "xlgllr4fcmc382d9t65jztugtka5s3",
+                              "streams?user_login=pokimane").json()["data"][0]["started_at"]
+    except IndexError:  # channel is offline or channel doesnt exist
+        uptime = None
+    else:
+        starttime = datetime.datetime.strptime(started, '%Y-%m-%dT%H:%M:%SZ')
+        uptime = datetime.datetime.utcnow() - starttime
+    return uptime
+
+
+def authtoken():
+    req = requests.post(
+        'https://id.twitch.tv/oauth2/token?client_id=xlgllr4fcmc382d9t65jztugtka5s3&client_secret=tdk6zmtuzno9ate5xjv0suvzcs09pp&grant_type=client_credentials')
+    print(req.text)
+
+
+def queen_is_live(api):
+    api.update_status(status='Our beloved queen is live on twitch! https://www.twitch.tv/pokimane')
+
+
+if __name__ == '__main__':
+    # print('Bot turning on')
+    twitter_api = create_twitter_api()
+    stream_tweeted = False
+
+    poki_status_bot = PokiStatusChecker(twitter_api)
+
+    while True:
+        if uptime() is not None and stream_tweeted is False:
+            stream_tweeted = True
+            queen_is_live(twitter_api)
+        elif uptime() is None:
+            stream_tweeted = False
+
+        like_at_mentions()
+        time_to_tweet(twitter_api)
+        poki_status_bot.check_twitter_status(twitter_api)
+        time.sleep(60 * 5)
+
+    # print('Bot turning off')
--- README.md
+++ README.md
@@ -1,2 +1,74 @@
 # Twitter Bot
- Personal project used to learn the Twitter API
+ Personal project used to learn the Twitter & Twitch API
+
+ This version of the bot is intentinally not going to have parts that will allow it to run. 
+
+Have access to twitch and twitter APIs
+
+Objective:
+    When Poki tweets [cannot be retweets], the bot will like, retweet and comment.
+    When Poki is online on twitch, the bot will tweet out that Poki is streaming.
+
+To Do:
+    - Import twitter api (done)
+        Tweepy
+    - Import twitch api (done)
+        twitchAPI
+    - Create stream that watching Poki's twitter (kinda)
+        > created a tweet puller that will check the recent tweets and compare if they have been tweeted before
+    - Create a general API config and boot up (done)
+    - Create a unique comment system that looks up keyphrases via Poki's Twitter (basic but done)
+    - Create a checker to see if Poki is streaming (done)
+    - Automatic generic tweets at set different times of the day (done)
+
+
+Auto tweeting
+    Based off of current computer time, it will automatically tweet about something
+        ie. 'Good morning fellow human simps!'
+
+Liking and retweeting process
+    Bot Pulls from Poki's Twitter's recent 30 tweets
+        -> Check if tweet is a retweet
+            if retweet then ignore
+            if tweet
+                -> retweets onto bot page
+                -> likes tweet
+                -> unique* comment
+                *unique comments requires me to pull and compare the tweet content for key words to determine what kind
+                response is needed for that tweet
+                    ex.
+                    Poki tweet below ▼
+                    ---------------------------------------------------------------------------------------------------
+                    if you've ever wanted a behind-the-scenes look at the business side of streaming,
+                    then check out my collab with graham stephan!
+
+                    link below~
+                    https://youtu.be/wvl05CQKkY0
+                    ---------------------------------------------------------------------------------------------------
+
+                    keyword for video tweets -> 'youtu.be'
+
+                    unique response -> 'Bless up, our queen has graced us with a video.
+                                        This was made from the simp bot gang.'
+
+
+Tweeting about Poki's Stream being online
+    Bot checks if Poki's Stream status
+        -> Check if she is online or offline
+            if offline do nothing
+            if online tweets
+                -> Tweets onto page saying 'Our queen is streaming. Go watch fellow simps!
+                                            https://www.twitch.tv/pokimane'
+
+Layout
+Poki Simp Bot/
+│
+├── config.py
+├── PokiTwitterTwitch.py
+│
+├── Bot_Info.txt
+└── Documentation.txt
+│
+└── runtime_info.log
+
+God bless Evan for all your help
